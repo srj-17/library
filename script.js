153fa23140e6874819375d6e951e999e4f97f883
srj-17 <saugatrijal17@gmail.com>
2024-07-31 16:42:20 +0545

    Complete basic styling and logic of the page

---
+++ script.js
@@ -0,0 +1,57 @@
+const myLibrary = [];
+const booksDashboard = document.querySelector('.books-dashboard');
+const deleteButtons = Array.from(document.querySelectorAll('.deleteButton'));
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return {
+            title: this.title,
+            author: this.author,
+            pages: this.pages,
+            read: this.read,
+        };
+    }
+};
+
+function addBook(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+};
+
+function displayBooks() {
+    myLibrary.forEach((book) => {
+        let bookCard = document.createElement('div');
+        bookCard.classList.toggle('book');
+        
+        bookInfo = book.info(); 
+        bookCard.innerHTML = `<div class="title">Title: ${book.title}</div>
+                            <div class="author">Author: ${book.author}</div>
+                            <div class="pages">Pages: ${book.title}</div>
+                            <div class="read">Read: ${book.read ? 'read' : 'not read'} </div>`;
+        
+        booksDashboard.appendChild(bookCard);
+                            
+        // add a delete button to each book
+        deleteButtonContainer = document.createElement('div');
+        deleteButtonContainer.classList.toggle('delete');
+        deleteButton = document.createElement('button');
+        deleteButton.classList.toggle('deleteButton');
+        deleteButton.textContent = 'Delete Book';
+        deleteButtonContainer.appendChild(deleteButton);
+        bookCard.appendChild(deleteButtonContainer);
+    });
+};
+
+function deleteBook(event) {
+    if (event.target in deleteButtons) {
+        
+    }
+}
+
+booksDashboard.addEventListener('click', (e) => {
+    deleteBook(e);
+});

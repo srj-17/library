420aaaadf399832ef7db69dd560f22bb33dc38c8
srj-17 <saugatrijal17@gmail.com>
2024-08-02 14:30:08 +0545

    Move Book.info() method to Book.prototype
    Prototypal inheritence saves memory

--- script.js
+++ script.js
@@ -18,15 +18,16 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
-        return {
-            title: this.title,
-            author: this.author,
-            pages: this.pages,
-            read: this.read,
-        };
-    }
 };
+
+Book.prototype.info = function() {
+    return {
+        title: this.title,
+        author: this.author,
+        pages: this.pages,
+        read: this.read,
+    };
+}
 
 // add book
 function addBook(title, author, pages, read) {
